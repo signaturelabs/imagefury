95940a7106016d13e6d4195b9ad5fa8167e71bf1
Traun Leyden <tleyden@branchcut.com>
2011-02-17 11:04:35 -0800

    Add license/copywrite header to all source files

--- Testproject/Classes/imagefuryViewController.h
+++ Testproject/Classes/imagefuryViewController.h
@@ -1,10 +1,18 @@
-//
-//  imagefuryViewController.h
-//  imagefury
-//
-//  Created by Traun Leyden on 2/10/11.
-//  Copyright 2011 __MyCompanyName__. All rights reserved.
-//
+/*
+ ``The contents of this file are subject to the Mozilla Public License
+ Version 1.1 (the "License"); you may not use this file except in
+ compliance with the License. You may obtain a copy of the License at
+ http://www.mozilla.org/MPL/
+ 
+ The Initial Developer of the Original Code is Hollrr, LLC.
+ Portions created by the Initial Developer are Copyright (C) 2011
+ the Initial Developer. All Rights Reserved.
+ 
+ Contributor(s):
+ 
+ Dustin Dettmer <dusty@dustytech.com>
+ Traun Leyden <tleyden@hollrr.com> 
+ */
 
 #import <UIKit/UIKit.h>
 
