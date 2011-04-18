851d1b6e63032770b98c79255dd11977258d8654
ddustin <dustinpaystaxes@gmail.com>
2011-04-18 16:55:21 -0700

    IFTextPlaceholder added

---
+++ ImageFury/Internals/IFTextPlaceholder.h
@@ -0,0 +1,27 @@
+/*
+ ``The contents of this file are subject to the Mozilla Public License
+ Version 1.1 (the "License"); you may not use this file except in
+ compliance with the License. You may obtain a copy of the License at
+ http://www.mozilla.org/MPL/
+ 
+ The Initial Developer of the Original Code is Hollrr, LLC.
+ Portions created by the Initial Developer are Copyright (C) 2011
+ the Initial Developer. All Rights Reserved.
+ 
+ Contributor(s):
+ 
+ Traun Leyden <tleyden@signature-app.com>
+ 
+ */
+
+
+#import <Foundation/Foundation.h>
+#import "IFPlaceholder.h"
+
+@interface IFTextPlaceholder : IFPlaceholder {
+
+}
+
+- (id)initWithText:(NSString*)text color:(UIColor *)color;
+
+@end
---
+++ ImageFury/Internals/IFTextPlaceholder.m
@@ -0,0 +1,100 @@
+/*
+ ``The contents of this file are subject to the Mozilla Public License
+ Version 1.1 (the "License"); you may not use this file except in
+ compliance with the License. You may obtain a copy of the License at
+ http://www.mozilla.org/MPL/
+ 
+ The Initial Developer of the Original Code is Hollrr, LLC.
+ Portions created by the Initial Developer are Copyright (C) 2011
+ the Initial Developer. All Rights Reserved.
+ 
+ Contributor(s):
+ 
+ Traun Leyden <tleyden@signature-app.com>
+ 
+ */
+
+#import "IFTextPlaceholder.h"
+
+@interface IFTextPlaceholder ()
+
+@property (nonatomic, retain) NSString *text;
+@property (nonatomic, retain) UIColor *color;
+
+@end
+
+
+@implementation IFTextPlaceholder
+
+@synthesize text, color;
+
+- (id)initWithText:(NSString*)str color:(UIColor *)col{
+
+	if (self = [super init]) {
+		
+		self.text = str;
+		self.color = col;
+	}
+	
+	return self;
+}
+
+- (UIView*)getPlaceholderGraphic:(UIView *)graphic {
+	
+	CGRect frame = CGRectZero;
+	
+	frame.size = self.frame.size;
+	
+	if(!graphic) {
+		
+		UILabel *lbl = [[[UILabel alloc] init] autorelease];
+		
+		lbl.frame = frame;
+		
+		lbl.autoresizingMask = UIViewAutoresizingFlexibleWidth
+		| UIViewAutoresizingFlexibleHeight;
+		
+		lbl.text = self.text;
+		lbl.opaque = NO;
+		lbl.textColor = color;
+		lbl.textAlignment = UITextAlignmentCenter;
+		lbl.backgroundColor = [UIColor clearColor];
+		lbl.font = [UIFont fontWithName:@"Helvetica" size:34];
+		lbl.adjustsFontSizeToFitWidth = YES;
+		lbl.alpha = 0;
+		
+		graphic = [[[UIView alloc] initWithFrame:frame] autorelease];
+		
+		[graphic addSubview:lbl];
+	}
+	
+	if(self.state == IFPlaceholderStateFailed) {
+		
+		[UIView beginAnimations:nil context:nil];
+		
+		[[graphic.subviews lastObject] setAlpha:1];
+		
+		[UIView commitAnimations];
+	}
+	else
+		[[graphic.subviews lastObject] setAlpha:0];
+	
+	graphic.frame = frame;
+	
+	return graphic;
+}
+
+- (UIView*)getLoadingIndicator:(UIView *)oldLoadingIndicator progress:(NSNumber *)progress {
+	
+	return nil;
+}
+
+- (void)dealloc {
+
+	self.text = nil;
+	self.color = nil;
+	
+	[super dealloc];
+}
+
+@end
